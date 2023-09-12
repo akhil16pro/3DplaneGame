315a63d05be7ebfd97bd0309d4f37232f31d778d
Akhil Peter George <Akhil16pro@gmail.com>
2023-09-12 15:09:39 +0400

    build for git page

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  return {
+    server: {
+      port: 3000,
+      hmr: true,
+      host: 'localhost',
+    },
+    preview: {
+      port: 8080,
+      open: true,
+    },
+
+    optimizeDeps: {
+      exclude: ['js-big-decimal'],
+    },
+    build: {
+      outDir: './docs/',
+
+      emptyOutDir: true,
+    },
+  }
+})
